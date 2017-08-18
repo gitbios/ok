e3fd7021f4b8e1afe1dccc51942f6aee88733558
gitbios <gibios@gmail.com>
2017-08-19 00:31:15 +0800

    ok

---
+++ fileproc/src/h.java
@@ -0,0 +1,7 @@
+public class h {
+	public static void main(String[] args) {
+		for (String str:args) {
+			System.out.println("s = " + str);
+		}
+	}
+}
---
+++ h.java
@@ -0,0 +1,7 @@
+public class h{
+public static void main(String []args){
+//String s = args[0];
+System.out.println("s = " + args[0]);
+System.out.println("s = " + args[1]);
+}
+}
